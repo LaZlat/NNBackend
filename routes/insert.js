cd0b5c12d162b0c714e23efd6b7b88999a17fd04
JonasPonas <zemaitis.jonas0@gmail.com>
2021-01-24 16:49:49 +0200

    kad ryciui veiktu...

--- routes/insert.js
+++ routes/insert.js
@@ -2,8 +2,8 @@ const express = require('express')
 const router = express.Router()
 
 router.post("/", function(req, res) {
-    let cl = req.query.cl;
-    let id = req.query.id;
+    let cl = req.body.class;
+    let id = req.body.id;
     db.query('UPDATE object SET class =' + cl + ' WHERE idobject = ' + id, function(err) {
         if (err) {
             return res.sendStatus(500);
